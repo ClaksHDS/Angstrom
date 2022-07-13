c127a072d3449b45c88c4fdc6d54b6167a828e01
Claks <huetclarisse2@gmail.com>
2022-07-13 11:15:28 +0100

    2nd commit
    Fixed footer & team sections.
    Add links to the Navbar & social icons to Contact section.
    Add & fixed JS open/close Navbar + fixed Navbar.

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-const navToggle = getElement('.nav-toggle');
-const links = getElement('.links');
+const getElement = (selector) => {
+  const el = document.querySelector(selector)
+  if (el) return el
+  throw new Error(`Please check your classes : ${selector} does not exist`)
+}
+
+const navToggle = getElement('.nav-toggle')
+const links = getElement('.links')
+const navBar = document.querySelector('#nav')
+
+//add fixed class to navbar
+window.addEventListener("scroll", function () {
+  if(this.window.pageYOffset > 80){
+    navBar.classList.add("navbar-fixed");
+  } else {
+    navBar.classList.remove("navbar-fixed");
+  }
+})
 
 navToggle.addEventListener('click', function () {
   links.classList.toggle('show-links')
