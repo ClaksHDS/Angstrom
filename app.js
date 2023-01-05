a6677abd815d6cfff1d7fc6ed3cc21df2b447b7c
[claks HDS] <you@example.com>
2023-01-05 12:47:40 +0000

    add logic to sidebar

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const navToggle = getElement(".nav-toggle");
 const links = getElement(".links");
 const navBar = document.querySelector("#nav");
 
-//add fixed class to navbar
+//add fixed class to navbar when scrolling down
 window.addEventListener("scroll", function () {
   if (this.window.pageYOffset > 80) {
     navBar.classList.add("navbar-fixed");
@@ -17,10 +17,15 @@ window.addEventListener("scroll", function () {
   }
 });
 
+// open sidebar when clicking on hamburger menu
 navToggle.addEventListener("click", function () {
   links.classList.toggle("show-links");
 });
 
+// close sidebar when clicking on a link
+links.addEventListener("click", function () {
+  links.classList.toggle("show-links");
+});
 // update the current year in the copyright footer
 const currentYear = new Date().getFullYear();
 
